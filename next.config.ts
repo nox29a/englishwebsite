e4e592fa4e10c8aa3ea500e3763dc0f7b62be9da
Łukasz Nox <lukasznox@MacBook-Pro-ukasz.local>
2025-08-05 21:51:57 +0200

    fixed config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  module.exports = {
   env: {
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
   }
-}
-
 };
 
 export default nextConfig;
